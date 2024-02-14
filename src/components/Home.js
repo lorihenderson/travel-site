b2f7f5c78d24cc06555b19952fb6057d9fadefdb
Lori Henderson <lorim95@gmail.com>
2024-02-14 15:17:47 -0500

    Fixes resort description container to show on separate lines

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div>
         <div id="home" className='resortDescriptionContainer'>
-            <h3 className='resortName'> Second-Rate Stays</h3>
-            <p className='resortSlogan'>We know we're not your first choice, but we love being your second.</p>
+            <h3 className='resortName'> Second-Rate Stays </h3>
+            <p className='resortSlogan'> We know we're not your first choice, but we love being your second. </p>
         </div>
         <div className='container-home'>
             <div className='homepageSlogan'>
-                <span className='slogan'>Your journey begins here.</span>
+                <span className='slogan'> Your journey begins here. </span>
             </div>
         </div>
     </div>
